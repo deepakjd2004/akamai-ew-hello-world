4eb878c3943f3898f7ebeb3763e612cd84d4769f
Deepak Jha <djha@akamai.com>
2025-04-08 16:59:26 +1000

    v12

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 export function onClientRequest(request) {
-  request.respondWith(200, {}, "<html><body><h1>Hello World from Akamai EdgeWorkers!</h1></body></html>");
+  request.respondWith(200, {}, "<html><body><h1>Hello World from Akamai EdgeWorkers, Yahoo!</h1></body></html>");
 }
